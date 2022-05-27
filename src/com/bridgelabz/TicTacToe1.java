ad8b8011391900a330b793b46bc6a6230a5e1248
PayaLsRaut <payalraut107@gmail.com>
2022-05-27 07:11:08 +0530

    Create Method And Check The Condition

--- src/com/bridgelabz/TicTacToe1.java
+++ src/com/bridgelabz/TicTacToe1.java
@@ -1,23 +1,43 @@
 package com.bridgelabz;
 
-public class TicTacToe1
-{
+import java.util*;
+import java.util.Scanner;
+
+public class TicTacToe1 {
     public static void main(String[] args)
-           {
-       // System.out.println("Welcome To Tic Tac Toe Workshop");
+    {
+        // System.out.println("Welcome To Tic Tac Toe Workshop");
 
-        char[] gameboard = createTTTBoard();
-            }
+        char[] board = createTTTBoard();
+        char UserLetter = chooseLetter();
+        char ComputerLetter;
+        if (UserLetter == 'X') {
+            ComputerLetter = 'O';
+        } else {
+            ComputerLetter = 'X';
 
-            public static char[] createTTTBoard()
-            {
-                char[] board = new char[10];
-                for(int index = 0; index < board.length; index++)
-                {
-                    board[index] = ' ';
-                }
-                return  board;
-            }
         }
+        System.out.println("Computer Letter is: " + ComputerLetter + " User Letter is : " + UserLetter);
+    }
+
+    public static char chooseLetter();
+
+    {
+        char symbol;
+        Scanner UserInput = new Scanner(System.in);
+        System.out.print("Enter your symbol");
+        symbol = UserInput.next().toUpperCase().charAt(0);
+        return symbol;
+    }
+
+    public static char[] createTTTBoard() {
+        char[] board = new char[10];
+        for (int index = 0; index < board.length; index++)
+        {
+            board[index] = ' ';
+        }
+        return board;
+    }
+}
 
 
