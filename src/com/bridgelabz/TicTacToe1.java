6f5e45a64114554a01b3e54d64662d4e10562276
PayaLsRaut <payalraut107@gmail.com>
2022-05-27 07:37:53 +0530

    Check Method To Display The Current Board

--- src/com/bridgelabz/TicTacToe1.java
+++ src/com/bridgelabz/TicTacToe1.java
@@ -1,27 +1,28 @@
 package com.bridgelabz;
 
-import java.util*;
+import java.util.*;
 import java.util.Scanner;
 
-public class TicTacToe1 {
+public class TicTacToe1
+{
     public static void main(String[] args)
     {
-        // System.out.println("Welcome To Tic Tac Toe Workshop");
-
-        char[] board = createTTTBoard();
+        char[] board = createBoard();
         char UserLetter = chooseLetter();
         char ComputerLetter;
-        if (UserLetter == 'X') {
-            ComputerLetter = 'O';
-        } else {
-            ComputerLetter = 'X';
+        if(UserLetter=='X')
+        {
+            ComputerLetter='O';
+        }
+        else
+        {
+            ComputerLetter='X';
 
         }
-        System.out.println("Computer Letter is: " + ComputerLetter + " User Letter is : " + UserLetter);
+        System.out.println("Computer Letter is: " + ComputerLetter+" User Letter is : " + UserLetter);
+        showBoard(board);
     }
-
-    public static char chooseLetter();
-
+    public static char chooseLetter()
     {
         char symbol;
         Scanner UserInput = new Scanner(System.in);
@@ -30,14 +31,22 @@ public class TicTacToe1 {
         return symbol;
     }
 
-    public static char[] createTTTBoard() {
+    public static void showBoard(char[] board)
+    {
+        System.out.println("\n" + board[1] + "  |  "  + board[2] + "  |  " + board[3]);
+        System.out.println("--------------");
+        System.out.println(" " + board[4] + "  |  "  + board[5] + "  |  " + board[6]);
+        System.out.println("--------------");
+        System.out.println(" " + board[7] + "  |  "  +board[8] + "  |  " + board[9]);
+        System.out.println("--------------");
+    }
+    public static char[] createBoard()
+    {
         char[] board = new char[10];
-        for (int index = 0; index < board.length; index++)
+        for(int index = 1; index < board.length; index++)
         {
             board[index] = ' ';
         }
-        return board;
+        return  board;
     }
 }
-
-
